37df3735d98bf3890dbce0e2d0e1569c89ff6078
Abhishek Gupta <abhishekg323@gmail.com>
2022-10-16 19:43:09 +0530

    refactored datamodel.js

--- datamodel.js
+++ datamodel.js
@@ -1,42 +1,38 @@
 module.exports = {
     room: null,
     started: false,
-    players: [
-    {
-        id: null,
-        name: null,
-        token: null,
-        notes:{
-            500:0,
-            100:0,
-            50:0,
-            10:0
-        },
-        owned:[{
-            position: 0,
-            name:null,
-            price:0,
-            rent:0,
-
-        }],
-    },
-    {
-        id: null,
-        name: null,
-        token: null,
-        notes:{
-            500:0,
-            100:0,
-            50:0,
-            10:0
-        },
-        owned:[{
-            position: 0,
-            name:null,
-            price:0,
-            rent:0,
-
-        }],
-    },
-]
-}
+    players: [{
+            id: null,
+            name: null,
+            token: null,
+            notes:{
+                500:0,
+                100:0,
+                50:0,
+                10:0
+            },
+            owned:[{
+                position: 0,
+                name:null,
+                price:0,
+                rent:0,
+            }],
+        },{
+            id: null,
+            name: null,
+            token: null,
+            notes:{
+                500:0,
+                100:0,
+                50:0,
+                10:0
+            },
+            owned:[{
+                position: 0,
+                name:null,
+                price:0,
+                rent:0,
+            }],
+        }
+    ]
+};
